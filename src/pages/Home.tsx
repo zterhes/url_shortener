3b6307a93e2b4e3a1750a26cf8c95d51ba1ef8fa
Zoran Terhes <zoran.terhes@rockwood.eu>
2023-08-09 21:09:25 +0200

    move images to src

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="py-10 pr-5 pl-5">
+        <Header />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        'cyan': '#29C7C7',
+        'darkViolet': '#3B3054'
+      },
+      secondary: {
+        'red': '#F46262'
+      },
+      neutral: {
+        'gray': '#BFBFBF',
+        'grayishViolet': '#9E9AA7',
+        'veryDarkBlue': '#35323E',
+        'veryDarkViolet': '#232127'
+      }
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins']
+      }
+    },
   },
   plugins: [],
 }
