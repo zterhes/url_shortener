ec0eb176fbef1183320ee0800a04ad211c7db2fc
Zoran Terhes <zoran.terhes@rockwood.eu>
2023-08-20 21:48:09 +0200

    create routing on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,7 @@ import { ReactComponent as PinterestLogo } from "../images/icon-pinterest.svg";
 import { ReactComponent as XLogo } from "../images/x-social-media-logo-icon.svg";
 import FooterBlockItem from "./FooterBlockItem";
 import Wrapper from "./Wrapper";
+import { Link } from "react-router-dom";
 
 const FOOTER_BLOCK_STYLE: string = "max-md:py-5";
 const FOOTER_TITLE_STYLE: string = "text-white pb-3";
@@ -23,42 +24,68 @@ const Footer = () => (
       className={FOOTER_BLOCK_STYLE}
       titleStyle={FOOTER_TITLE_STYLE}
     >
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Link Shortening" />
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Branded Links" />
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Analytics" />
+      <FooterBlockItem
+        className={FOOTER_ITEM_STYLE}
+        to={"#"}
+        title="Link Shortening"
+      />
+      <FooterBlockItem
+        className={FOOTER_ITEM_STYLE}
+        to={"#"}
+        title="Branded Links"
+      />
+      <FooterBlockItem
+        className={FOOTER_ITEM_STYLE}
+        to={"#"}
+        title="Analytics"
+      />
     </Wrapper>
     <Wrapper
       title={"Resources"}
       className={FOOTER_BLOCK_STYLE}
       titleStyle={FOOTER_TITLE_STYLE}
     >
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Blog" />
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Developers" />
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Support" />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} to={"#"} title="Blog" />
+      <FooterBlockItem
+        className={FOOTER_ITEM_STYLE}
+        to={"#"}
+        title="Developers"
+      />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} to={"#"} title="Support" />
     </Wrapper>
     <Wrapper
       title={"Company"}
       className={FOOTER_BLOCK_STYLE}
       titleStyle={FOOTER_TITLE_STYLE}
     >
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="About" />
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Our Team" />
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Careers" />
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Contact" />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} to={"#"} title="About" />
+      <FooterBlockItem
+        className={FOOTER_ITEM_STYLE}
+        to={"#"}
+        title="Our Team"
+      />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} to={"#"} title="Careers" />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} to={"#"} title="Contact" />
     </Wrapper>
     <div className="w-full flex mt-7 h-7 justify-center">
-      <div className={FOOTER_SOCIAL_ICON_STYLE}>
+      <Link
+        to={"https://www.facebook.com/"}
+        className={FOOTER_SOCIAL_ICON_STYLE}
+      >
         <FacebookLogo />
-      </div>
-      <div className={FOOTER_SOCIAL_ICON_STYLE}>
+      </Link>
+      <Link to={"https://x.com/"} className={FOOTER_SOCIAL_ICON_STYLE}>
         <XLogo fill="#FFFFFF" />
-      </div>
-      <div className={FOOTER_SOCIAL_ICON_STYLE}>
+      </Link>
+      <Link to={"https://pinterest.com/"} className={FOOTER_SOCIAL_ICON_STYLE}>
         <PinterestLogo />
-      </div>
-      <div className={FOOTER_SOCIAL_ICON_STYLE}>
+      </Link>
+      <Link
+        to={"https://www.instagram.com/"}
+        className={FOOTER_SOCIAL_ICON_STYLE}
+      >
         <InstagramLogo />
-      </div>
+      </Link>
     </div>
   </div>
 );
--- src/components/FooterBlockItem.tsx
+++ src/components/FooterBlockItem.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface Props {
   title: string;
   className: string;
+  to: string;
 }
 
-const FooterBlockItem: React.FC<Props> = ({ title, className }) => {
-  return <p className={className}>{title}</p>;
+const FooterBlockItem: React.FC<Props> = ({ title, className, to }) => {
+  return (
+    <Link to={to}>
+      <p className={className}>{title}</p>
+    </Link>
+  );
 };
 
 export default FooterBlockItem;
