34174fa46c0d1701c88c7a9d53562bc7e9793f81
Zoran Terhes <zoran.terhes@rockwood.eu>
2023-08-20 20:32:50 +0200

    style blockItems

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,53 +5,41 @@ import FooterBlockItem from "./FooterBlockItem";
 import Wrapper from "./Wrapper";
 
 const FOOTER_BLOCK_STYLE: string = "max-md:py-5";
+const FOOTER_TITLE_STYLE: string = "text-white pb-3";
+const FOOTER_ITEM_STYLE: string = "text-neutral-grayishViolet pt-2";
+
 const Footer = () => (
-  <div className="bg-neutral-veryDarkBlue max-md:text-center md:flex ">
-    <div className="justify-center">
+  <div className="bg-neutral-veryDarkBlue max-md:text-center md:flex py-10 ">
+    <div className="w-full flex justify-center mb-5">
       <Logo imageRoute={ShortlyLogo} alt="Shortly logo" />
     </div>
     <Wrapper
       title={"Features"}
       className={FOOTER_BLOCK_STYLE}
-      titleStyle={"text-white"}
+      titleStyle={FOOTER_TITLE_STYLE}
     >
-      <FooterBlockItem
-        className="text-neutral-grayishViolet"
-        title="Link Shortening"
-      />
-      <FooterBlockItem
-        className="text-neutral-grayishViolet"
-        title="Branded Links"
-      />
-      <FooterBlockItem
-        className="text-neutral-grayishViolet"
-        title="Analytics"
-      />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Link Shortening" />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Branded Links" />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Analytics" />
     </Wrapper>
     <Wrapper
       title={"Resources"}
       className={FOOTER_BLOCK_STYLE}
-      titleStyle={"text-white"}
+      titleStyle={FOOTER_TITLE_STYLE}
     >
-      <FooterBlockItem className="text-neutral-grayishViolet" title="Blog" />
-      <FooterBlockItem
-        className="text-neutral-grayishViolet"
-        title="Developers"
-      />
-      <FooterBlockItem className="text-neutral-grayishViolet" title="Support" />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Blog" />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Developers" />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Support" />
     </Wrapper>
     <Wrapper
       title={"Company"}
       className={FOOTER_BLOCK_STYLE}
-      titleStyle={"text-white"}
+      titleStyle={FOOTER_TITLE_STYLE}
     >
-      <FooterBlockItem className="text-neutral-grayishViolet" title="About" />
-      <FooterBlockItem
-        className="text-neutral-grayishViolet"
-        title="Our Team"
-      />
-      <FooterBlockItem className="text-neutral-grayishViolet" title="Careers" />
-      <FooterBlockItem className="text-neutral-grayishViolet" title="Contact" />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="About" />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Our Team" />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Careers" />
+      <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Contact" />
     </Wrapper>
   </div>
 );
