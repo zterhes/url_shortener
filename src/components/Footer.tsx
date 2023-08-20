9d439c5f5d811b3898921be5f41d20320293c259
Zoran Terhes <zoran.terhes@rockwood.eu>
2023-08-20 20:57:19 +0200

    finish footer design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Logo from "./Logo";
-import ShortlyLogo from "../images/logo.svg";
+import { ReactComponent as ShortlyLogo } from "../images/logo.svg";
+import { ReactComponent as InstagramLogo } from "../images/icon-instagram.svg";
+import { ReactComponent as FacebookLogo } from "../images/icon-facebook.svg";
+import { ReactComponent as PinterestLogo } from "../images/icon-pinterest.svg";
+import { ReactComponent as XLogo } from "../images/x-social-media-logo-icon.svg";
 import FooterBlockItem from "./FooterBlockItem";
 import Wrapper from "./Wrapper";
 
 const FOOTER_BLOCK_STYLE: string = "max-md:py-5";
 const FOOTER_TITLE_STYLE: string = "text-white pb-3";
 const FOOTER_ITEM_STYLE: string = "text-neutral-grayishViolet pt-2";
+const FOOTER_SOCIAL_ICON_STYLE: string = "mx-3";
 
 const Footer = () => (
   <div className="bg-neutral-veryDarkBlue max-md:text-center md:flex py-10 ">
     <div className="w-full flex justify-center mb-5">
-      <Logo imageRoute={ShortlyLogo} alt="Shortly logo" />
+      <ShortlyLogo fill="#FFFFFF" />
     </div>
     <Wrapper
       title={"Features"}
@@ -41,6 +46,20 @@ const Footer = () => (
       <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Careers" />
       <FooterBlockItem className={FOOTER_ITEM_STYLE} title="Contact" />
     </Wrapper>
+    <div className="w-full flex mt-7 h-7 justify-center">
+      <div className={FOOTER_SOCIAL_ICON_STYLE}>
+        <FacebookLogo />
+      </div>
+      <div className={FOOTER_SOCIAL_ICON_STYLE}>
+        <XLogo fill="#FFFFFF" />
+      </div>
+      <div className={FOOTER_SOCIAL_ICON_STYLE}>
+        <PinterestLogo />
+      </div>
+      <div className={FOOTER_SOCIAL_ICON_STYLE}>
+        <InstagramLogo />
+      </div>
+    </div>
   </div>
 );
 
