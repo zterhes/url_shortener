669d81fabf34925da1032a50fae54c019d8fbe1e
Zoran Terhes <zoran.terhes@rockwood.eu>
2023-09-29 08:08:24 +0200

    implementation finishing footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,64 +9,78 @@ import FooterBlockItem from "./FooterBlockItem";
 import Wrapper from "./Wrapper";
 import { Link } from "react-router-dom";
 
-const FOOTER_BLOCK_STYLE: string = "max-md:py-5";
-const FOOTER_TITLE_STYLE: string = "text-white pb-3";
-const FOOTER_ITEM_STYLE: string = "text-neutral-grayishViolet pt-2";
-const FOOTER_SOCIAL_ICON_STYLE: string = "mx-3";
+const FOOTER_BLOCK_STYLE = "";
+const FOOTER_TITLE_STYLE = "text-white pb-3";
+const FOOTER_ITEM_STYLE = "text-neutral-grayishViolet pt-2";
+const FOOTER_SOCIAL_ICON_STYLE = "mx-3";
 
 const Footer = () => (
   <div className="bg-neutral-veryDarkBlue max-md:text-center md:flex py-10 ">
     <div className="w-full flex justify-center mb-5">
       <ShortlyLogo fill="#FFFFFF" />
     </div>
-    <Wrapper
-      title={"Features"}
-      className={FOOTER_BLOCK_STYLE}
-      titleStyle={FOOTER_TITLE_STYLE}
-    >
-      <FooterBlockItem
-        className={FOOTER_ITEM_STYLE}
-        to={"#"}
-        title="Link Shortening"
-      />
-      <FooterBlockItem
-        className={FOOTER_ITEM_STYLE}
-        to={"#"}
-        title="Branded Links"
-      />
-      <FooterBlockItem
-        className={FOOTER_ITEM_STYLE}
-        to={"#"}
-        title="Analytics"
-      />
-    </Wrapper>
-    <Wrapper
-      title={"Resources"}
-      className={FOOTER_BLOCK_STYLE}
-      titleStyle={FOOTER_TITLE_STYLE}
-    >
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} to={"#"} title="Blog" />
-      <FooterBlockItem
-        className={FOOTER_ITEM_STYLE}
-        to={"#"}
-        title="Developers"
-      />
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} to={"#"} title="Support" />
-    </Wrapper>
-    <Wrapper
-      title={"Company"}
-      className={FOOTER_BLOCK_STYLE}
-      titleStyle={FOOTER_TITLE_STYLE}
-    >
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} to={"#"} title="About" />
-      <FooterBlockItem
-        className={FOOTER_ITEM_STYLE}
-        to={"#"}
-        title="Our Team"
-      />
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} to={"#"} title="Careers" />
-      <FooterBlockItem className={FOOTER_ITEM_STYLE} to={"#"} title="Contact" />
-    </Wrapper>
+    <div className="flex flex-col md:flex-row gap-5">
+      <Wrapper
+        title={"Features"}
+        className={FOOTER_BLOCK_STYLE}
+        titleStyle={FOOTER_TITLE_STYLE}
+      >
+        <FooterBlockItem
+          className={FOOTER_ITEM_STYLE}
+          to={"#"}
+          title="Link Shortening"
+        />
+        <FooterBlockItem
+          className={FOOTER_ITEM_STYLE}
+          to={"#"}
+          title="Branded Links"
+        />
+        <FooterBlockItem
+          className={FOOTER_ITEM_STYLE}
+          to={"#"}
+          title="Analytics"
+        />
+      </Wrapper>
+      <Wrapper
+        title={"Resources"}
+        className={FOOTER_BLOCK_STYLE}
+        titleStyle={FOOTER_TITLE_STYLE}
+      >
+        <FooterBlockItem className={FOOTER_ITEM_STYLE} to={"#"} title="Blog" />
+        <FooterBlockItem
+          className={FOOTER_ITEM_STYLE}
+          to={"#"}
+          title="Developers"
+        />
+        <FooterBlockItem
+          className={FOOTER_ITEM_STYLE}
+          to={"#"}
+          title="Support"
+        />
+      </Wrapper>
+      <Wrapper
+        title={"Company"}
+        className={FOOTER_BLOCK_STYLE}
+        titleStyle={FOOTER_TITLE_STYLE}
+      >
+        <FooterBlockItem className={FOOTER_ITEM_STYLE} to={"#"} title="About" />
+        <FooterBlockItem
+          className={FOOTER_ITEM_STYLE}
+          to={"#"}
+          title="Our Team"
+        />
+        <FooterBlockItem
+          className={FOOTER_ITEM_STYLE}
+          to={"#"}
+          title="Careers"
+        />
+        <FooterBlockItem
+          className={FOOTER_ITEM_STYLE}
+          to={"#"}
+          title="Contact"
+        />
+      </Wrapper>
+    </div>
     <div className="w-full flex mt-7 h-7 justify-center">
       <Link
         to={"https://www.facebook.com/"}
@@ -74,7 +88,7 @@ const Footer = () => (
       >
         <FacebookLogo />
       </Link>
-      <Link to={"https://x.com/"} className={FOOTER_SOCIAL_ICON_STYLE}>
+      <Link to={"https://www.x.com/"} className={FOOTER_SOCIAL_ICON_STYLE}>
         <XLogo fill="#FFFFFF" />
       </Link>
       <Link to={"https://pinterest.com/"} className={FOOTER_SOCIAL_ICON_STYLE}>
